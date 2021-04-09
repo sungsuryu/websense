4afb5c47bd40e13afad9c3912e9b12a341298b73
sungsuryu <sungsu.ryu@gmail.com>
2021-04-09 11:20:10 +0900

    websense initialize

---
+++ websense/src/main/java/com/isensoft/websense/demo/WebsenseApplication.java
@@ -0,0 +1,13 @@
+package com.isensoft.websense.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsenseApplication.class, args);
+	}
+
+}
---
+++ websense/src/test/java/com/isensoft/websense/demo/WebsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isensoft.websense.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
